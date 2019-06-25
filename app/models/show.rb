ee6921cd8ed4d358d88c55effdde222d5e1a7e90
Andrew Davisson <akdavisson4@gmail.com>
2019-06-25 20:18:43 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
+  def build_network(:call_letters)
+    Network.new(:call_letters)
+  end
+  
 end
